4b2bc5f580a6cd6c55c385d2a59ae15cc9075e94
Zobrino <china8st@gmail.com>
2025-07-06 14:44:27 -0700

    Update main.py

--- main.py
+++ main.py
@@ -30,3 +30,6 @@ async def translate_audio(req: CallRequest):
 
     except Exception as e:
         return {"error": str(e)}
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app", host="0.0.0.0", port=8000)
